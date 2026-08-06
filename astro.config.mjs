f8b3447ce8bff5ab456d0e44c0e3b72e84c17dcb
DESKTOP-68PFDJP\jiusongPC11 <15795498+yangze9264@user.noreply.gitee.com>
2026-08-06 16:18:02 +0800

    fix: use npx astro build and add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   vite: {
     resolve: {
       alias: {
